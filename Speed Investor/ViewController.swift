61af74129a9970308c18cd989d9aee64e092d189
user151107 <user151107@92-63-141-213.rdns.melbourne.co.uk>
2019-02-25 09:24:12 +0000

    Initial Commit

---
+++ Speed Investor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speed Investor
+//
+//  Created by user151107 on 2/25/19.
+//  Copyright © 2019 user151107. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
